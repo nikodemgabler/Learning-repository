5a460cde719d37b3c6d14504e9e897c2ee045541
Kacper Walter <kacper.walter@netguru.com>
2022-03-22 22:10:52 +0100

    code review

--- NG-scss/js/main.js
+++ NG-scss/js/main.js
@@ -1,6 +1,7 @@
 const navMenu = document.querySelector(".navbar__menu")
 const hamburger = document.querySelector(".navbar__burger")
 const navButton = document.querySelector(".navbar__button")
+// nie lapiemy JSem rzeczy po klasach htmlowych, klasy są dla CSS. Do JSa uzywamy data-attributes https://javascript.plainenglish.io/quick-guide-to-using-data-attributes-f1f2c3161d5f
 
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("open")
